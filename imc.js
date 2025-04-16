a1f706d5ea069e912c3c0c8f06c463aed3268d53
André Ricardo <ricardoandredias@gmail.com>
2025-04-16 15:44:29 -0300

    Update imc.js

--- imc.js
+++ imc.js
@@ -1,25 +1,25 @@
-função imc(){
+function imc(){
     const formulario = document.getElementsByName("formulario")[0]
-    const formDados = new FormData(formulário)
+    const formDados = new FormData(formulario)
     const paciente = {
         nome:formDados.get("nome"),
         email:formDados.get("email"),
         dataNasc:formDados.get("nasc"),
         telefone:formDados.get("tel"),
-        sexo:document.querySelector('input[nome=sexo]:checked').value,
+        sexo:document.querySelector('input[name=sexo]:checked').value,
         peso:formDados.get("peso"),
         altura:formDados.get("altura"),
-        classificação:"",
+        classificacao:"",
         calcularIMC(){
             if (this.altura > 3) this.altura=this.altura/100
-            const imc = isto.peso/isto.altura**2;
-            if (imc < 18.5 ) this.classificacao = "obesidade mórbida";
-            if (imc >= 18.5 && imc < 25 ) this.classificacao = "saudável"
-            if (imc >= 25 && imc < 30 ) this.classificacao = "sobrepeso"
-            if (imc >= 30 && imc < 35 ) this.classificacao = "obesidade"
-            if (imc >= 35 && imc < 40 ) this.classificacao = "obesidade Severa"
+            const imc = this.peso/this.altura**2;
+            if (imc < 18.5 ) this.classificacao = "obesidade mÃ³rbida";
+            if (imc >= 18.5 &&  imc < 25 ) this.classificacao = "saudÃ¡vel"
+            if (imc >= 25 &&  imc < 30 ) this.classificacao = "sobrepeso"
+            if (imc >= 30 &&  imc < 35 ) this.classificacao = "obesidade"
+            if (imc >= 35 &&  imc < 40 ) this.classificacao = "obesidade Severa"
             if (imc >= 40 ) this.classificacao = "magreza"
-            retornar imc.toFixed(2);
+            return imc.toFixed(2);
         
             
             
@@ -28,6 +28,6 @@ função imc(){
     }
     console.log(paciente)
     console.log(paciente.calcularIMC())
-    deixe resposta=document.getElementById('resposta');
-    resposta.innerHTML="Seu IMC é "+paciente.calcularIMC()+". " + "Sua classificação de peso é " + paciente.classificacao;
+    let resposta=document.getElementById('resposta');
+    resposta.innerHTML="Seu IMC Ã© "+paciente.calcularIMC()+". " + "Sua classificaÃ§Ã£o de peso Ã© " + paciente.classificacao;
 }
